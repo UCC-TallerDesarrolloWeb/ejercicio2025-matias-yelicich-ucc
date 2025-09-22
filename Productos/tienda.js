db2ee33fb2f57842878741bac5d0deed60721dc2
matias-yelicich-ucc <2415049@ucc.edu.ar>
2025-09-22 08:48:51 -0300

    Ahora se puede filtrar por precio

--- Productos/tienda.js
+++ Productos/tienda.js
@@ -1,93 +1,94 @@
 const productos = [
-  {
-    nombre: "Cabezal Sparring",
-    description: "Cabezal de Sparring.",
-    categoria: "Protectores",
-    marca: "Gran Marc",
-    talle: ["1", "2", "3"],
-    precio: 35000,
-    web: "https://www.granmarctiendaonline.com.ar/productos/cabezal-cerrado/",
-    imagen: "cabezal-cerrado.webp",
-  },
-  {
-    nombre: "Dobok Dan",
-    description: "Bobok aprobado para torneos internacionales.",
-    categoria: "Dobok",
-    marca: "Daedo",
-    talle: ["1", "2", "3", "4", "5", "6", "7", "8"],
-    precio: 115000,
-    web: "https://www.daedo.com/products/taitf-10813",
-    imagen: "dobok.webp",
-  },
-  {
-    nombre: "Escudo de Potencia",
-    description: "Escudo de potencia para entrenamientos.",
-    categoria: "Entrenamiento",
-    marca: "Gran Marc",
-    talle: ["s/talle"],
-    precio: 51700,
-    web: "https://www.granmarctiendaonline.com.ar/productos/escudo-de-potencia-grande/",
-    imagen: "escudo-potencia.webp",
-  },
-  {
-    nombre: "Par de focos redondos",
-    description: "Par de focos de 25cm x 25cm para hacer entrenamiento.",
-    categoria: "Entrenamiento",
-    marca: "Gran Marc",
-    talle: ["s/talle"],
-    precio: 15000,
-    web: "https://www.granmarctiendaonline.com.ar/productos/foco-con-dedos/",
-    imagen: "foco-con-dedos.webp",
-  },
-  {
-    nombre: "Guantes 10 onzas",
-    description:
-      "Guantes de Sparring de 10 onzas habilitados para torneos internacionales",
-    categoria: "Protectores",
-    marca: "Daedo",
-    talle: ["s/talle"],
-    precio: 35000,
-    web: "https://www.daedo.com/products/pritf-2020",
-    imagen: "protectores-manos.webp",
-  },
-  {
-    nombre: "Protectores Pie",
-    description: "Protectores de Pie habilitados para torneos internacionales",
-    categoria: "Protectores",
-    marca: "Daedo",
-    talle: ["XXS", "XS", "S", "M", "L", "XL"],
-    precio: 35000,
-    web: "https://www.daedo.com/collections/collection-itf-gloves/products/pritf-2022",
-    imagen: "protectores-pie.webp",
-  },
+    {
+        nombre: "Cabezal Sparring",
+        description: "Cabezal de Sparring.",
+        categoria: "Protectores",
+        marca: "Gran Marc",
+        talle: ["1", "2", "3"],
+        precio: 35000,
+        web: "https://www.granmarctiendaonline.com.ar/productos/cabezal-cerrado/",
+        imagen: "cabezal-cerrado.webp",
+    },
+    {
+        nombre: "Dobok Dan",
+        description: "Bobok aprobado para torneos internacionales.",
+        categoria: "Dobok",
+        marca: "Daedo",
+        talle: ["1", "2", "3", "4", "5", "6", "7", "8"],
+        precio: 115000,
+        web: "https://www.daedo.com/products/taitf-10813",
+        imagen: "dobok.webp",
+    },
+    {
+        nombre: "Escudo de Potencia",
+        description: "Escudo de potencia para entrenamientos.",
+        categoria: "Entrenamiento",
+        marca: "Gran Marc",
+        talle: ["s/talle"],
+        precio: 51700,
+        web: "https://www.granmarctiendaonline.com.ar/productos/escudo-de-potencia-grande/",
+        imagen: "escudo-potencia.webp",
+    },
+    {
+        nombre: "Par de focos redondos",
+        description: "Par de focos de 25cm x 25cm para hacer entrenamiento.",
+        categoria: "Entrenamiento",
+        marca: "Gran Marc",
+        talle: ["s/talle"],
+        precio: 15000,
+        web: "https://www.granmarctiendaonline.com.ar/productos/foco-con-dedos/",
+        imagen: "foco-con-dedos.webp",
+    },
+    {
+        nombre: "Guantes 10 onzas",
+        description:
+        "Guantes de Sparring de 10 onzas habilitados para torneos internacionales",
+        categoria: "Protectores",
+        marca: "Daedo",
+        talle: ["s/talle"],
+        precio: 35000,
+        web: "https://www.daedo.com/products/pritf-2020",
+        imagen: "protectores-manos.webp",
+    },
+    {
+        nombre: "Protectores Pie",
+        description: "Protectores de Pie habilitados para torneos internacionales",
+        categoria: "Protectores",
+        marca: "Daedo",
+        talle: ["XXS", "XS", "S", "M", "L", "XL"],
+        precio: 35000,
+        web: "https://www.daedo.com/collections/collection-itf-gloves/products/pritf-2022",
+        imagen: "protectores-pie.webp",
+    },
 ];
 
 let mostrarDetalles = (id) => {
-  document.getElementById("detalles").style.display = "block";
-  document.getElementById("detalles__titulo").innerText = productos[id].nombre;
-  document.getElementById("detalles__descripcion").innerText = productos[id].description;
-  document.getElementById("detalles__precio").innerText = `$${productos[id].precio}`;
+    document.getElementById("detalles").style.display = "block";
+    document.getElementById("detalles__titulo").innerText = productos[id].nombre;
+    document.getElementById("detalles__descripcion").innerText = productos[id].description;
+    document.getElementById("detalles__precio").innerText = `$${productos[id].precio}`;
 }
 
 let cerrarModal = () => {
-  document.getElementById("detalles").style.display = "none";
+    document.getElementById("detalles").style.display = "none";
 }
 
-let mostrarCatalogo = () => {
-  let contenido = "";
+let mostrarCatalogo = (prod = productos) => {
+    let contenido = "";
 
-  productos.forEach((prod, id) => {
-    contenido += `
-      <div>
-        <img src="imagenes/${prod.imagen}" alt="${prod.nombre}" />
-        <h3>${prod.nombre}</h3>
-        <button type="button" onclick="mostrarDetalles(${id})">Ver Detalles</button>
-        <button type="button" onclick="agregarCarrito(${id})">Agregar al carrito</button>
-      </div>
-    `;
-  })
+    prod.forEach((prod, id) => {
+        contenido += `
+        <div>
+            <img src="imagenes/${prod.imagen}" alt="${prod.nombre}" />
+            <h3>${prod.nombre}</h3>
+            <p>$${prod.precio}</p>
+            <button type="button" onclick="mostrarDetalles(${id})">Ver Detalles</button>
+            <button type="button" onclick="agregarCarrito(${id})">Agregar al carrito</button>
+        </div>
+        `;
+    })
 
-  document.getElementById("lista-productos").innerHTML = contenido;
+    document.getElementById("lista-productos").innerHTML = contenido;
 }
 
 let agregarCarrito = (id) => {
@@ -115,7 +116,7 @@ let mostrarListaCarrito = () => {
         <div>
             <h3>${productos[num].nombre}</h3>
             <p>$${productos[num].precio}</p>
-            <button type="button" onclick="eliminarProducto(${id}})">Eliminar producto</button>
+            <button type="button" onclick="eliminarProducto(${id})">Eliminar producto</button>
         </div>
         `;
     })
@@ -143,4 +144,34 @@ let eliminarProducto = (id) => {
     }
 
     window.location.reload()
+}
+
+let filtrarProducto = () => {
+    let searchWord = document.getElementById("search").value;
+    let min = document.getElementById("price-min").value;
+    let max = document.getElementById("price-max").value;
+    let prot = document.getElementById("protectores").checked;
+    let entr = document.getElementById("entrenamiento").checked;
+    let dob = document.getElementById("dobok").checked;
+
+    let newLista = productos;
+
+    if(searchWord) {
+        newLista = newLista.filter(
+            (prod) => 
+                prod.nombre.toLocaleLowerCase().includes(searchWord.toLocaleLowerCase()) ||
+                prod.description.toLocaleLowerCase().includes(searchWord.toLocaleLowerCase())
+        );
+    }
+    
+    if(min) {
+        newLista = newLista.filter((prod) => prod.precio >= min)
+    }
+    if(max) {
+        newLista = newLista.filter((prod) => prod.precio <= max)
+    }
+
+    
+
+    mostrarCatalogo(newLista)
 }
