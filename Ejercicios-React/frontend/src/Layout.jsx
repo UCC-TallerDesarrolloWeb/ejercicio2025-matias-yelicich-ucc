0ab72c6c4c0e8d16ce43a971edd6651927bc25eb
matias-yelicich-ucc <2415049@ucc.edu.ar>
2025-10-13 11:48:15 -0300

    Se corrigieron las rutas

--- Ejercicios-React/frontend/src/Layout.jsx
+++ Ejercicios-React/frontend/src/Layout.jsx
@@ -1,9 +1,16 @@
+import { Outlet } from "react-router-dom";
 import Header from "./Header";
+import Footer from "./Footer";
 
 const Layout = () => {
-
     return (
-        <Header />
+        <>
+            <Header />
+            <main>
+                <Outlet />
+            </main>
+            <Footer />
+        </>
     )
 }
 
--- Ejercicios-React/frontend/src/main.jsx
+++ Ejercicios-React/frontend/src/main.jsx
@@ -5,13 +5,18 @@ import './styles/index.css'
 import App from './App.jsx'
 import Login from './Login.jsx'
 import Activities from './Activities.jsx'
+import Layout from './Layout.jsx';
+import Home from './Home.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login />}/>
-          <Route path="/actividades" element={<Activities />}/>
+          <Route path='/' element={ <Layout/> }>
+            <Route index element={<Home />}/>
+            <Route path="/login" element={<Login />}/>
+            <Route path="/actividades" element={<Activities />}/>
+          </Route>
         </Routes>
       </BrowserRouter>
   </StrictMode>,
