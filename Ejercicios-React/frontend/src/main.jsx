950de52ad60db6891c151df8eefa1e21077eb69e
matias-yelicich-ucc <2415049@ucc.edu.ar>
2025-10-13 11:20:30 -0300

    se agrego la pagina actividades

--- Ejercicios-React/frontend/src/main.jsx
+++ Ejercicios-React/frontend/src/main.jsx
@@ -1,8 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactDOM from "react-dom/client";
 import './styles/index.css'
 import App from './App.jsx'
 import Login from './Login.jsx'
+import Activities from './Activities.jsx'
+
+const root = document.getElementById("");
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="./Activities.jsx" element={<Activities />} />
+    </Routes>
+  </BrowserRouter>,
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
