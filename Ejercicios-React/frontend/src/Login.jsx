2c74c8d5f09006ef5e8632bdd3631b7b7c94d627
matias-yelicich-ucc <2415049@ucc.edu.ar>
2025-10-13 09:40:17 -0300

    ejercicio login

---
+++ Ejercicios-React/frontend/src/Login.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import "./login.css"
+
+const Login = () => {
+    const [usuario, setUsuario] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handlerLogin = async (e) => {
+        e.preventDefault();
+
+        if(usuario=="admin" && password=="admin") {
+            console.log("Login OK");
+        } else {
+            console.log("Login ERROR");
+        }
+    }
+
+    return (
+        <div className="login-container">
+            <form className="login-form" onSubmit={handlerLogin}>
+                <h2>Iniciar Sesión</h2>
+                <label htmlFor="user">Usuario:</label>
+                <input 
+                    id="user"
+                    type="text" 
+                    placeholder="Usuario" 
+                    value={usuario} 
+                    onChage={(e) => setUsuario(e.target.value)} 
+                    required
+                />
+                <label htmlFor="pass">Contraseña:</label>
+                <input 
+                    id="pass"
+                    type="text"
+                    placeholder="Constraseña"
+                    value={password}
+                    onChage={(e) => setPassword(e.target.value)} 
+                    required
+                />
+                <button type="submit">Ingresar</button>
+            </form>
+        </div>
+    )
+}
+
+export default Login;
